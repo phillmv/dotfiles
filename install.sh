5b710a638770806f1efd48e4a21e6031e672756e
Phill MV <phillmv@github.com>
2021-11-08 11:22:40 -0500

    codespaces install

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+exec > >(tee -i $HOME/dotfiles_install.log)
+exec 2>&1
+set -x
+
+#ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
+ln -s $(pwd)/.vimrc $HOME/.vimrc
+ln -s $(pwd)/vim $HOME/.vim
+#ln -s $(pwd)/emacs $HOME/.emacs
+#ln -s $(pwd)/screenrc $HOME/.screenrc
+git submodule update --init
