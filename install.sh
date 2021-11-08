776756d03dc4102f19b9f91f407c7dec3e0b572c
Phill MV <phillmv@github.com>
2021-11-08 16:13:44 -0500

    add vim-js, fix .vim symlink

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set -x
 
 #ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/.vimrc $HOME/.vimrc
-ln -s $(pwd)/vim $HOME/.vim
+ln -s $(pwd)/.vim $HOME/.vim
 #ln -s $(pwd)/emacs $HOME/.emacs
 #ln -s $(pwd)/screenrc $HOME/.screenrc
 git submodule update --init
