a4a643524c78bfee4de9acfd1cf99df92c0e40f9
Phill MV <phillmv@github.com>
2021-12-02 15:23:55 -0500

    what if we fork the setup?

--- install.sh
+++ install.sh
@@ -4,10 +4,4 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
-#ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
-ln -s $(pwd)/.vimrc $HOME/.vimrc
-ln -s $(pwd)/.vim $HOME/.vim
-#ln -s $(pwd)/emacs $HOME/.emacs
-#ln -s $(pwd)/screenrc $HOME/.screenrc
-git submodule update --init;
-vim +'PlugInstall' +qa;
+./script/setup &
---
+++ script/setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+exec > >(tee -a -i $HOME/dotfiles_install.log)
+exec 2>&1
+set -x
+
+#ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
+ln -s $(pwd)/.vimrc $HOME/.vimrc
+ln -s $(pwd)/.vim $HOME/.vim
+#ln -s $(pwd)/emacs $HOME/.emacs
+#ln -s $(pwd)/screenrc $HOME/.screenrc
+git submodule update --init;
+vim +'PlugInstall' +qa;
