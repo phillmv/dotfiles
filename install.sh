6961de57720022696b23f25661c82e7313d6d97a
Phill MV <phillmv@github.com>
2021-12-02 10:57:13 -0500

    if we add ;s, will that prevent codespaces from dying should these commands return an error?

--- install.sh
+++ install.sh
@@ -9,5 +9,5 @@ ln -s $(pwd)/.vimrc $HOME/.vimrc
 ln -s $(pwd)/.vim $HOME/.vim
 #ln -s $(pwd)/emacs $HOME/.emacs
 #ln -s $(pwd)/screenrc $HOME/.screenrc
-git submodule update --init
-vim +'PlugInstall' +qa
+git submodule update --init;
+vim +'PlugInstall' +qa;
