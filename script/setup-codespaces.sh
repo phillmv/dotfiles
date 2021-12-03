6b74dfafdc28a8bb04364a4b185b5b47804412fa
Phill MV <phillmv@github.com>
2021-12-03 14:51:21 -0500

    ensure vim gets installed, thnx

--- script/setup-codespaces.sh
+++ script/setup-codespaces.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git config --global url."https://github.com/".insteadOf git@github.com:
-
+apt-get install -y vim
 mkdir -p ~/.ssh
 # could use ${GITHUB_USER}, but it's-a-me, phillmv
 curl --silent --fail "https://github.com/phillmv.keys" > ~/.ssh/authorized_keys
