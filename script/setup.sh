324751fc5c01c41e2da5f0323e7dd8484da2e93b
Phill MV <phillmv@github.com>
2021-12-10 16:22:50 -0500

    what if tmux?

--- script/setup.sh
+++ script/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
+ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/.vimrc $HOME/.vimrc
 ln -s $(pwd)/.vim $HOME/.vim
 #ln -s $(pwd)/emacs $HOME/.emacs
