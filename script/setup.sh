bfb86783de416e9280eefc41d5de5ab130967230
Phill MV <phillmv@github.com>
2022-03-08 10:22:35 -0500

    add my minimal bash_profile

--- script/setup.sh
+++ script/setup.sh
@@ -3,6 +3,7 @@
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/.vimrc $HOME/.vimrc
 ln -s $(pwd)/.vim $HOME/.vim
+ln -s $(pwd)/.bash_profile $HOME/.bash_profile
 #ln -s $(pwd)/emacs $HOME/.emacs
 #ln -s $(pwd)/screenrc $HOME/.screenrc
 git submodule update --init;
