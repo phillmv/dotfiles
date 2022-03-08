b82d26cdc5dd3a8f0be17089b2932b64756dc87f
Phill MV <phillmv@github.com>
2022-03-08 10:22:24 -0500

    install ripper tags in codespaces

--- script/setup-codespaces.sh
+++ script/setup-codespaces.sh
@@ -2,6 +2,7 @@
 
 git config --global url."https://github.com/".insteadOf git@github.com:
 apt-get install -y vim
+gem install ripper-tags
 mkdir -p ~/.ssh
 # could use ${GITHUB_USER}, but it's-a-me, phillmv
 curl --silent --fail "https://github.com/phillmv.keys" > ~/.ssh/authorized_keys
