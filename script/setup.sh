ae1d6e98ca4b9123a87bf0c53ea74c7479836c01
Phill MV <phillmv@github.com>
2023-03-22 16:18:26 -0400

    copy over nvim conf to right place.

--- script/setup.sh
+++ script/setup.sh
@@ -4,7 +4,8 @@ ln -s $(pwd)/tmux.conf     $HOME/.tmux.conf
 ln -s $(pwd)/.vimrc        $HOME/.vimrc
 ln -s $(pwd)/.vim          $HOME/.vim
 ln -s $(pwd)/.bash_profile $HOME/.bash_profile
-ln -s $(pwd)/.config       $HOME/.config
+mkdir -p $HOME/.config
+ln -s $(pwd)/.config/nvim  $HOME/.config
 #ln -s $(pwd)/emacs $HOME/.emacs
 #ln -s $(pwd)/screenrc $HOME/.screenrc
 git submodule update --init;
